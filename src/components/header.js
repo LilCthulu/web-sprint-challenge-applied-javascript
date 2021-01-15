0efe8d598f6bbc9937b9da62b39ef71beb035429
Chaz Carbis <chazcarbis@gmail.com>
2021-01-15 12:55:14 -0500

    myp

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,51 @@
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+    const head = document.createElement('div')
+    const dated = document.createElement('span')
+    const titled = document.createElement('h1')
+    const temped = document.createElement('span')
+
+    head.appendChild(date)
+    head.appendChild(title)
+    head.appendChild(temp)
+
+    head.classList.add('header')
+    dated.classList.add('date')
+    temped.classList.add('temp')
+
+    dated.textContent = date
+    titled.textContent = title
+    temped.textContent = temp
+
+    return head;
+    // TASK 1
+    // ---------------------
+    // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+    // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+    // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+    //
+    //  <div class="header">
+    //    <span class="date">{ date }</span>
+    //    <h1>{ title }</h1>
+    //    <span class="temp">{ temp }</span>
+    //  </div>
+    //
 }
 
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
-}
+    const newHeader = Header('Test', '16.6.99', '98°F');
+    const target = document.querySelector(selector)
+    target.appendChild({ newHeader });
+    console.log('success');
+
+
+    // TASK 2
+    // ---------------------
+    // Implement this function taking a css selector as its only argument.
+    // It should create a header using the Header component above, passing arguments of your choosing.
+    // It should append the header to the element in the DOM that matches the given selector.
+    //
+};
+
+
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,64 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+    const topic = document.createElement('div');
+    const tab1 = document.createElement('div');
+    const tab2 = document.createElement('div');
+    const tab3 = document.createElement('div');
+    const tab4 = document.createElement('div');
+    const tab5 = document.createElement('div');
+
+
+    topic.appendChild(tab1)
+    topic.appendChild(tab2)
+    topic.appendChild(tab3)
+    topic.appendChild(tab4)
+    topic.appendChild(tab5)
+
+    topic.classList.add('topics')
+    tab1.classList.add('tab')
+    tab2.classList.add('tab')
+    tab3.classList.add('tab')
+    tab4.classList.add('tab')
+    tab5.classList.add('tab')
+
+    tab1.textContent = topics[0]
+    tab2.textContent = topics[1]
+    tab3.textContent = topics[2]
+    tab4.textContent = topics[3]
+    tab5.textContent = topics[4]
+    return topic
+        // TASK 3
+        // ---------------------
+        // Implement this function which takes an array of strings ("topics") as its only argument.
+        // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+        // then the function returns the markup below.
+        // The tags used, the hierarchy of elements and their attributes must match the provided markup!
+        // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+        //
+        // <div class="topics">
+        //   <div class="tab">javascript</div>
+        //   <div class="tab">bootstrap</div>
+        //   <div class="tab">technology</div>
+        // </div>
+        //
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+    axios
+        .get('https://lambda-times-api.herokuapp.com/topics')
+        .then(res => {
+            const newTabs = Tabs(res.data.topics)
+            document.querySelector(selector).appendChild({ newTabs })
+        })
+        .catch(err => console.log(err))
+        // TASK 4
+        // ---------------------
+        // Implement this function which takes a css selector as its only argument.
+        // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
+        // Find the array of topics inside the response, and create the tabs using the Tabs component.
+        // Append the tabs to the element in the DOM that matches the selector passed to the function.
+        //
 }
 
 export { Tabs, tabsAppender }
